9b10e2ebd173709b3ef22e6678e188b595f99628
Ross Ward <micward2@gmail.com>
2015-08-17 03:11:25 +0100

    fix tab spaces issue

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,49 +1,49 @@
 class TodosController < ApplicationController
-	def index
-		@todos = Todo.all
-		@todos = Todo.order(:updated_at).page params[:page]
-	end
+  def index
+    @todos = Todo.all
+    @todos = Todo.order(:updated_at).page params[:page]
+  end
 
-	def show
-		@todo = Todo.find(params[:id])
-	end
+  def show
+    @todo = Todo.find(params[:id])
+  end
 
-	def new
-		@todo = Todo.new
-	end
+  def new
+    @todo = Todo.new
+  end
 
-	def edit
-		@todo = Todo.find(params[:id])
-	end
+  def edit
+    @todo = Todo.find(params[:id])
+  end
 
-	def create
-		@todo = Todo.new(todos_params)
-		
-		if @todo.save
-		  redirect_to todos_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    @todo = Todo.new(todos_params)
 
-	def update
-		@todo = Todo.find(params[:id])
+    if @todo.save
+      redirect_to todos_path
+    else
+      render 'new'
+    end
+  end
 
-		if @todo.update(todos_params)
-			redirect_to todos_path
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @todo = Todo.find(params[:id])
 
-	def destroy
-		@todo = Todo.find(params[:id])
-		@todo.destroy
-		redirect_to todos_path
-	end
+    if @todo.update(todos_params)
+      redirect_to todos_path
+    else
+      render 'edit'
+    end
+  end
 
-	private
-	  def todos_params
-	  	params.require(:todo).permit(:title, :description)
-	  end
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def todos_params
+    params.require(:todo).permit(:title, :description)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	# Helper for formatting the last modified field
-	def pretty_date(d)
-		d.strftime('%B %e, %Y %H:%M')
-	end
+  # Helper for formatting the last modified field
+  def pretty_date(d)
+    d.strftime('%B %e, %Y %H:%M')
+  end
 end
